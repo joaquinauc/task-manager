077cf67cbdff94bdd5a05bdc46aa82563360fe60
joaquinauc <joaquins_urrea@hotmail.com>
2025-02-24 04:37:53 -0600

    add register view function to routes file

--- app/routes.py
+++ app/routes.py
@@ -5,7 +5,7 @@ from urllib.parse import urlsplit
 import sqlalchemy as sa
 
 from app import app, db
-from app.forms import LoginForm
+from app.forms import LoginForm, RegistrationForm
 from app.models import User
 
 
@@ -45,3 +45,18 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for('index'))
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
