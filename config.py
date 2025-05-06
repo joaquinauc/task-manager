c81c16ffd331f4845ec2c37d45332862c7b62a37
joaquinauc <joaquins_urrea@hotmail.com>
2025-05-06 17:19:11 -0600

    add function set_sqlite_pragma to make ondelete='CASCADE' work

--- config.py
+++ config.py
@@ -1,7 +1,18 @@
 import os
+from sqlalchemy import event
+from sqlalchemy.engine import Engine
+import sqlite3
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
+
+
+@event.listens_for(Engine, "connect")
+def set_sqlite_pragma(dbapi_connection, connection_record):
+    if isinstance(dbapi_connection, sqlite3.Connection):
+        cursor = dbapi_connection.cursor()
+        cursor.execute("PRAGMA foreign_keys=ON;")
+        cursor.close()
